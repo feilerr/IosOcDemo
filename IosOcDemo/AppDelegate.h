07c7b98240af4e146401160d59f91ca63cf5ca87
feilerr <feilerr@126.com>
2016-10-29 19:06:31 +0800

    1.0.0
    创建一个Obective-c版本的框架

---
+++ IosOcDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  IosOcDemo
+//
+//  Created by Zhou on 2016/10/29.
+//  Copyright © 2016年 Zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
